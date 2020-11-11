623809e445f5392809e981f0cb2d02e397db4641
FediMejri <mejri.fedi.95@gmail.com>
2020-11-11 21:32:51 +0100

    Handlel auth

--- app.js
+++ app.js
@@ -1,11 +1,17 @@
 const express = require('express')
 const mongoose = require('mongoose')
+const auth=require('./routes/auth')
+const passport=require('passport')
+
+require('./config/passport')(passport)
 
 const app= express()
 
 app.get('/',(req,res)=>{
     res.send('it works')
 })
+
+app.use('/auth',auth)
 
 const port=process.env.PORT || 5001
 
---
+++ config/keys.js
@@ -0,0 +1,5 @@
+module.exports = {
+    mongoURI : 'mongodb+srv://mohamedfedi:abcdefg1234!@cluster0-5q2tv.mongodb.net/ShortStories?retryWrites=true&w=majority',
+    googleClientID : '930295896081-498vs9m6tln9tvg29nmhj4641sd7q7ob.apps.googleusercontent.com',
+    googleClientSecret : 'emyuN0aOo8kb5TePFaeNeFD7'
+}
