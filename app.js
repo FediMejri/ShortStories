5288a17f19b10445432da169622c49fa1260358b
FediMejri <mejri.fedi.95@gmail.com>
2020-11-13 19:52:10 +0100

    Started UI

--- app.js
+++ app.js
@@ -1,16 +1,47 @@
 const express = require('express')
 const mongoose = require('mongoose')
+const cookieParser= require('cookie-parser')
+const session = require('express-session')
 const auth=require('./routes/auth')
+const index=require('./routes/index')
+const exphbs=require('express-handlebars')
 const passport=require('passport')
+const keys = require('./config/keys')
+require('./models/User')
 
 require('./config/passport')(passport)
 
-const app= express()
-
-app.get('/',(req,res)=>{
-    res.send('it works')
+mongoose.Promise=global.Promise
+mongoose.connect(keys.mongoURI,{
+    useNewUrlParser: true 
+})
+.then(()=>{console.log('database connected')})
+.catch(err=>{
+    console.log(err)
 })
 
+const app= express()
+
+app.engine('handlebars',exphbs({
+    defaultLayout:'main'
+}))
+app.set('view engine','handlebars')
+
+app.use(cookieParser())
+app.use(session({
+    secret:'secret',
+    resave:false,
+    saveUninitialized:false
+}))
+app.use(passport.initialize())
+app.use(passport.session())
+
+app.use((req,res,next)=>{
+    res.locals.user = req.user || null
+    next()
+})
+
+app.use('/',index)
 app.use('/auth',auth)
 
 const port=process.env.PORT || 5001
---
+++ routes/index.js
@@ -0,0 +1,12 @@
+const express = require('express')
+const router = express.Router()
+
+router.get('/',(req,res)=>{
+    res.render('index/welcome')
+})
+
+router.get('/dashbord',(req,res)=>{
+    res.send('dashbord')
+})
+
+module.exports=router
