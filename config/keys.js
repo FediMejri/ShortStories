f229b5cf9221259e70766fe83c2d31b47efbc2dd
Mohamed Fedi Mejri <43710844+FediMejri@users.noreply.github.com>
2020-11-15 16:40:10 +0100

    Update keys.js

--- config/keys.js
+++ config/keys.js
@@ -1,5 +1,5 @@
 module.exports = {
-    mongoURI : 'mongodb+srv://mohamedfedi:abcdefg1234!@cluster0-5q2tv.mongodb.net/ShortStories?retryWrites=true&w=majority',
-    googleClientID : '930295896081-498vs9m6tln9tvg29nmhj4641sd7q7ob.apps.googleusercontent.com',
-    googleClientSecret : 'emyuN0aOo8kb5TePFaeNeFD7'
+    mongoURI : 'This is confidential',
+    googleClientID : 'This is confidential',
+    googleClientSecret : 'This is confidential'
 }
